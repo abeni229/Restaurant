1808266301c0a4f5d32b335c55c16e1c072f6f03
abeni229 <Gazaliouroukayath@gmail.com>
2026-05-08 19:25:36 +0100

    new

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VitrineController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\PlatController;
 
-// Page d'accueil du site vitrine (publique)
-Route::get('/', [VitrineController::class, 'accueil'])->name('accueil');
-
-// Pages supplémentaires du site vitrine (publiques)
-Route::get('/contact', [VitrineController::class, 'contact'])->name('contact');
-Route::get('/a-propos', [VitrineController::class, 'aPropos'])->name('a-propos');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/statistiques', [DashboardController::class, 'statistiques'])->name('statistiques');
-    Route::get('/parametres', [DashboardController::class, 'parametres'])->name('parametres');
-    Route::get('/profil/modifier', [DashboardController::class, 'modifierProfil'])->name('profil.modifier');
-});
-
-
-// Traitement du formulaire de contact (POST)
-Route::post('/contact', [ContactController::class, 'store'])->name('contact.send');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::resource('menus', MenuController::class);
+Route::resource('reservations', ReservationController::class);
+Route::get('/notre-carte', [PlatController::class, 'index'])->name('carte');
